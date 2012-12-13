cf93d764ff294670b5e9a99d39232756a21d8bc3
Tim Cellar <tcellar1@gmail.com>
2012-12-12 20:45:29 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cft::Application.config.secret_token = 'c7dd27de631cdf18d3a2270ce55d1fcd34efa774bf3e00aa8d4718f7421df91d934a72ae143f7d8acd6850dd725196bfcc78ac9207e04179a20b68366cccab98'
